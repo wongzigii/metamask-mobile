0221cf44ec6cc8737cb76378fb079303862afbbb
Curtis <Curtis.David7@gmail.com>
2023-04-26 00:12:29 -0400

    fix config file

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -4,9 +4,7 @@ module.exports = {
   testMatch: ['<rootDir>/e2e/specs/*.spec.js'],
   testTimeout: 120000,
   maxWorkers: 1,
-  transform: {
-    '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
-  },
+  setupFilesAfterEnv: ['<rootDir>/e2e/init.js'],
   globalSetup: 'detox/runners/jest/globalSetup',
   globalTeardown: 'detox/runners/jest/globalTeardown',
   reporters: ['detox/runners/jest/reporter'],

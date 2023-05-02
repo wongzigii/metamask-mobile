8734526b3ce2659ffa3eca92b07aab0045351b31
Mpendulo Ndlovu <mpendulo@elefantel.com>
2023-05-02 15:18:21 +0200

    Fix AndroidPackage compile errors

--- android/app/src/main/java/io/metamask/nativeModules/AndroidPackage.kt
+++ android/app/src/main/java/io/metamask/nativeModules/AndroidPackage.kt
@@ -1,4 +1,4 @@
-package io.metamask
+package io.metamask.nativeModules
 
 import android.view.View
 import com.facebook.react.ReactPackage
@@ -6,13 +6,12 @@ import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ReactShadowNode
 import com.facebook.react.uimanager.ViewManager
+import com.google.android.gms.common.util.CollectionUtils.listOf
+
+import io.metamask.CommunicationClient
 
 class AndroidPackage : ReactPackage {
-    override fun createViewManagers(
-        reactContext: ReactApplicationContext
-    ): MutableList<ViewManager<View, ReactShadowNode<*>>> = mutableListOf()
+  override fun createNativeModules(reactContext: ReactApplicationContext): MutableList<NativeModule> = listOf(CommunicationClient(reactContext)).toMutableList()
 
-    override fun createNativeModules(
-        reactContext: ReactApplicationContext
-    ): MutableList<NativeModule> = listOf(CommunicationClient(reactContext)).toMutableList()
+  override fun createViewManagers(reactContext: ReactApplicationContext): MutableList<ViewManager<View, ReactShadowNode<*>>> = mutableListOf()
 }

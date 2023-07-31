16686d7065cf3d8dd74a80d0fb1b45811b6f1fb1
Vinicius Stevam <vinicius.stevam@consensys.net>
2023-07-31 21:16:16 +0100

    port forward only android

--- e2e/init.js
+++ e2e/init.js
@@ -7,7 +7,7 @@ import {
 beforeAll(async () => {
   // Avoid port forwarding in Bitrise
   const isRunningBitrise = process.env.BITRISE_APP_TITLE;
-  if (!isRunningBitrise) {
+  if (!isRunningBitrise || device.getPlatform() === 'android') {
     await device.reverseTcpPort(12345);
   }
   // Start server cause all E2E test relies on the new infrastructure for fixtures once IS_TEST is `true`

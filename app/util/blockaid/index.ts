b7003014edce1e91f5a82d06e24bd591bf901ca6
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2023-10-09 10:53:51 +0100

    Add metrics for blockaid provider calls count.

--- app/util/blockaid/index.ts
+++ app/util/blockaid/index.ts
@@ -8,13 +8,23 @@ import {
 export const isBlockaidFeatureEnabled = () =>
   process.env.MM_BLOCKAID_UI_ENABLED === 'true';
 
+export const PPOM_CALL_METRIC_KEY_MAP: Record<string, string> = {
+  eth_call: 'ppom_eth_call_count',
+  eth_createAccessList: 'ppom_eth_createAccessList_count',
+  eth_getStorageAt: 'ppom_eth_getStorageAt_count',
+  eth_getCode: 'ppom_eth_getCode_count',
+  eth_getTrasanctionCount: 'ppom_eth_getTrasanctionCount_count',
+  eth_getBalance: 'ppom_eth_getBalance_count',
+  trace_call: 'ppom_trace_call_count',
+};
+
 export const getBlockaidMetricsParams = (
   securityAlertResponse: SecurityAlertResponse,
 ) => {
   const additionalParams: Record<string, any> = {};
 
   if (securityAlertResponse && isBlockaidFeatureEnabled()) {
-    const { resultType, reason } = securityAlertResponse;
+    const { resultType, reason, providerRequestsCount } = securityAlertResponse;
     let uiCustomizations;
 
     if (resultType === ResultType.Malicious) {
@@ -30,6 +40,14 @@ export const getBlockaidMetricsParams = (
         additionalParams.security_alert_response = resultType;
         additionalParams.security_alert_reason = reason;
       }
+    }
+
+    // add counts of each RPC call
+    if (providerRequestsCount) {
+      Object.keys(providerRequestsCount).forEach((key: string) => {
+        const metricKey = PPOM_CALL_METRIC_KEY_MAP[key];
+        additionalParams[metricKey] = providerRequestsCount[key];
+      });
     }
   }
 
